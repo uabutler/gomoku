f7261924e85051e78c3a7ac9dcf26c20cb7314e4
uabulter@live.com <uab@red.localdomain>
2020-03-30 01:34:07 -0500

    combined into one class

--- board.cpp
+++ board.cpp
@@ -1,38 +1,202 @@
 /* By Ulysses A. Butler */
 
 #include <vector>
-#include <random>
-#include <chrono>
-
 #include <iostream>
 
 #include "board.h"
 
+#define TOP "---"
+#define SIDE "|"
+#define INTER "+"
+
 // Constructor
-GomokuBoard::GomokuBoard(size_t s): size(s)
+Board::Board(size_t s): size(s)
 {
-  squares = vector<vector<square>>(s, vector<square>(s, empty));
+  height = (s * 2) + 1;
+  width = (s * 4) + 1; 
+  board = vector<vector<square>>(s, vector<square>(s, empty));
 }
 
 // Getter
-size_t GomokuBoard::getSize()
+int Board::getHeight()
+{
+  return height;
+}
+
+int Board::getWidth()
+{
+  return width;
+}
+
+size_t Board::getSize()
 {
   return size;
 }
 
-// Check to see if a square has a bomb
-square GomokuBoard::getSquare(size_t r, size_t c)
+square Board::getSquareAt(size_t r, size_t c)
 {
-  return squares[r][c];
+  return board[r][c];
 }
 
-void GomokuBoard::setSquare(size_t r, size_t c, square piece)
+void Board::setSquareAt(size_t r, size_t c, square s)
 {
-  squares[r][c] = piece;
+  char center;
+
+  if(s == x_piece) center = 'x';
+  if(s == o_piece) center = 'o';
+  if(s == empty) center = ' ';
+
+  int oldRow = cursorRow, oldCol = cursorCol;
+  moveCursor(r, c);
+  printw("%c", center);;
+  moveCursor(oldRow, oldCol);
+
+  board[r][c] = s;
 }
 
-bool GomokuBoard::checkWin()
+void Board::print(int r, int c)
+{
+  size_t i;
+  startRow = r;
+  startCol = c;
+
+  // Print top
+  move(startRow, startCol);
+
+  for(i = 0; i < size; i++)
+    printw(INTER TOP); 
+
+  printw(INTER);
+
+  // Print sides
+  for(i = startRow + 1; i < (2 * size) + (startRow + 1); i += 2)
+  {
+    mvprintw(i, startCol, SIDE);
+    mvprintw(i + 1, startCol, INTER);
+
+    mvprintw(i, startCol + (width - 1), SIDE);
+    mvprintw(i + 1, startCol + (width - 1), INTER);
+  }
+
+  // Print bottom
+  move(i - 1, startCol);
+
+  for(i = 0; i < size; i++)
+    printw(INTER TOP);
+
+  // Print intersections
+  for(size_t r = 0; r < size; r++)
+  {
+    for(size_t c = 0; c < size; c++)
+    {
+      mvprintw((2 * r) + 2 + startRow, (4 * c) + 4 + startCol, INTER);
+    }
+  }
+
+  // Print sides
+  for(size_t r = 0; r < size; r++)
+  {
+    for(size_t c = 0; c < size; c++)
+    {
+      mvprintw((2 * r) + 1 + startRow, (4 * c) + 4 + startCol, SIDE);
+    }
+  }
+
+  // Print top and bottom
+  for(size_t r = 0; r < size; r++)
+  {
+    for(size_t c = 0; c < size; c++)
+    {
+      mvprintw((2 * r) + 2 + startRow, (4 * c) + 1 + startCol, TOP);
+    }
+  }
+
+  moveCursor(0, 0);
+}
+
+square Board::getSquare()
+{
+  return board[cursorRow][cursorCol];
+}
+
+void Board::update(square s)
+{
+  char center;
+
+  if(s == x_piece) center = 'x';
+  if(s == o_piece) center = 'o';
+  if(s == empty) center = ' ';
+
+  moveCursor(cursorRow, cursorCol);
+  printw("%c", center);;
+  moveCursor(cursorRow, cursorCol);
+
+  refresh();
+}
+
+int Board::getRow()
+{
+  return cursorRow;
+}
+
+int Board::getCol()
+{
+  return cursorCol;
+}
+
+void Board::moveCursor(int r, int c)
+{
+  cursorRow = r;
+  cursorCol = c;
+
+  move((2 * r) + 1 + startRow, (4 * c) + 2 + startCol);
+  refresh();
+}
+
+bool Board::checkWin()
 {
   // TODO
   return false;
+}
+
+void Board::getUserMove(square piece)
+{
+  bool flag = true;
+  int input;
+  
+  while(flag)
+  {
+    while(((input = getch()) != '\n') && (input != KEY_ENTER))
+    {
+      int newRow = cursorRow;
+      int newCol = cursorCol;
+      flag = false;
+
+      if(input == KEY_UP)
+        newRow--;
+      else if(input == KEY_DOWN)
+        newRow++;
+      else if(input == KEY_RIGHT) 
+        newCol++;
+      else if(input == KEY_LEFT)
+        newCol--;
+      else
+        flag = true;
+
+      if(newRow >= size || newRow < 0 || newCol >= size || newCol < 0)
+        flag = true;
+      else
+        moveCursor(newRow, newCol);
+    }
+
+    mvprintw(0, 0, "1");
+    refresh();
+
+    if(getSquare() != empty)
+      flag = true;
+
+    if(flag) cout << '\a';
+  }
+
+  update(piece);
 }
--- board.h
+++ board.h
@@ -3,27 +3,62 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include <ncurses.h>
 #include <vector>
 
 using namespace std;
 
 enum square {empty, x_piece, o_piece};
 
-class GomokuBoard
+class Board
 {
- private:
-  vector<vector<square>> squares;
+private:
+  vector<vector<square>> board;
+
+  int startRow;
+  int startCol;
+
+  int cursorRow;
+  int cursorCol;
 
   size_t size;
 
- public:
-  GomokuBoard(size_t s);
+  int height;
+  int width;
 
+public:
+  // First, the constructor is initialized with a reference to the
+  // board the user will manage
+  Board(size_t s);
+
+  // The height and width of the printed board. This isn't the same
+  // as the number of squares on the board
+  int getHeight();
+  int getWidth();
+
+  // The size of the underlying board. A 10x10 board has a size of 10
   size_t getSize();
-  
-  square getSquare(size_t r, size_t c);
-  void setSquare(size_t r, size_t c, square piece);
+
+  // Using indexes
+  square getSquareAt(size_t r, size_t c);
+  void setSquareAt(size_t r, size_t c, square piece);
+
+  // The initial print of the board at the given coordinates
+  void print(int r, int c);
+  // This retrieves the current square
+  square getSquare();
+  // Updates the board and moves the cursor to the given coordinates
+  void update(square peice);
+  // Get the current location of the cursor, relative to the board
+  int getRow();
+  int getCol();
+  // Moves the cursor the the given coordinates without updating the
+  // whole board
+  void moveCursor(int r, int c);
+  // Has a player won the game
   bool checkWin();
+  // play game
+  void getUserMove(square piece);
 };
 
 #endif // BOARD_H
